cc1c9748f624be5e45c53b181e322151f05eee8b
Samuel <samuelurbinaflor@gmail.com>
2026-01-24 14:06:50 +0100

    counter reset on timeout

--- scripts/gui/gui.gd
+++ scripts/gui/gui.gd
@@ -97,6 +97,9 @@ func start_timer() -> void:
 func _on_time_finished() -> void:
 	_hide_and_stop_ui()
 	get_tree().paused = false
+	corrupted_count = 9
+	_update_mushroom_counter()
+	
 	# Solo recargar si estamos en un nivel
 	if has_level_scene():
 		get_tree().reload_current_scene()
